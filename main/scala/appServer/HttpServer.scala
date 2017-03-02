3523ed8d57af86a1473d7609cdcae62d3d1424e7
Sergio Rodríguez Calvo <sergiorodriguezcalvo@gmail.com>
2017-03-02 07:51:46 +0100

    #1 Hello Simple Server at localhost:8080/hello

--- main/scala/appServer/HttpServer.scala
+++ main/scala/appServer/HttpServer.scala
@@ -1,8 +1,40 @@
 package appServer
 
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.server.Directives._
+import scala.io.StdIn
+//import com.typesafe.config._
+
 /**
   * Created by serrodcal on 1/3/17.
   */
-object HttpServer {
+object HttpServer extends App {
+
+  override def main(args: Array[String]): Unit = {
+
+    implicit val system = ActorSystem("my-system")
+    implicit val materializer = ActorMaterializer()
+
+    // needed for the future flatMap/onComplete in the end
+    implicit val executionContext = system.dispatcher
+
+    val route = path("hello") {
+          get {
+            complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>"))
+          }
+     }
+
+    val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
+
+    println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
+    StdIn.readLine() // let it run until user presses return
+    bindingFuture
+      .flatMap(_.unbind()) // trigger unbinding from the port
+      .onComplete(_ => system.terminate()) // and shutdown when done
+
+  }
 
 }
