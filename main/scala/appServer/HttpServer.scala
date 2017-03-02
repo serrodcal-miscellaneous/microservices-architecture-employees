808580b5934a0fa995e8fbe95266c68ebb4a0ca8
Sergio Rodríguez Calvo <sergiorodriguezcalvo@gmail.com>
2017-03-02 08:04:18 +0100

    #1 Hello Simple Server for POST request at localhost:8080/echo

--- main/scala/appServer/HttpServer.scala
+++ main/scala/appServer/HttpServer.scala
@@ -5,6 +5,8 @@ import akka.stream.ActorMaterializer
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.Route
+
 import scala.io.StdIn
 //import com.typesafe.config._
 
@@ -21,8 +23,15 @@ object HttpServer extends App {
     // needed for the future flatMap/onComplete in the end
     implicit val executionContext = system.dispatcher
 
-    val route = path("hello") {
-          get {
+    val route : Route = post {
+        path("echo") {
+          complete((StatusCodes.Accepted, "Recived"))
+        }
+      }
+
+
+      path("echo") {
+          post {
             complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>"))
           }
      }
