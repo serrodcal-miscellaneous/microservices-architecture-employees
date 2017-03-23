d13b37a6688509ab14855761bc6f70de138d53e2
Sergio Rodríguez Calvo <sergiorodriguezcalvo@gmail.com>
2017-03-23 23:07:33 +0100

    Fix EmployeeRouteTest creating an ActorRef for test

--- src/test/scala/routes/EmployeeRoutesTest.scala
+++ src/test/scala/routes/EmployeeRoutesTest.scala
@@ -1,17 +1,29 @@
 package routes
 
+import actors.EmployeeEchoActor
+import akka.actor.{ActorSystem, Props}
 import akka.event.Logging
 import akka.http.scaladsl.model.{ContentTypes, HttpEntity, StatusCodes}
 import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.testkit.ScalatestRouteTest
+import akka.stream.ActorMaterializer
+import com.typesafe.config.ConfigFactory
 import org.scalatest.{Matchers, WordSpec}
+import server.HttpServer.getClass
 
 /**
   * Created by serrodcal on 15/3/17.
   */
 class EmployeeRoutesTest extends WordSpec with Matchers with ScalatestRouteTest {
 
+  val config = ConfigFactory.load()
+
+  // needed for the future flatMap/onComplete in the end
+  implicit val executionContext = system.dispatcher
+
   implicit val logger = Logging(system, getClass)
+
+  implicit val employeeEchoActor = system.actorOf(Props[EmployeeEchoActor], name = "employeeEchoActor")
 
   val employeeRoutes = new EmployeeRoutes()
 
